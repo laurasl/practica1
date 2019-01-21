5d2b7b3dee282074bd360bc7ae3150715d9ffc64
Laura <laurasanchezzlopezz@gmail.com>
2019-01-21 16:24:38 +0100

    hola2

--- ddg.py
+++ ddg.py
@@ -1,6 +1,6 @@
 jugador = int(input("Introduzca un número"))
 def ddg(jugador):
-    jugadores = ["Juan", "Jose", "Maria", "Pedro"]
+    jugadores = ["Juan", "Jose", "Maria", "Pedro", "Lorena"]
     for i in range(len(jugadores)):
         if i == jugador:
             return(jugadores[i])
